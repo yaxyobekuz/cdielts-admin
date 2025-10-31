4559ce456069cd714250dfd3504c2b62496987d9
yaxyobekuz <pubgn9642@gmail.com>
2025-10-31 19:17:24 +0500

    feat: add data files for allowed image types, assessment criteria, gradients, navigation links, permissions, and question types

---
+++ src/data/navlinks.js
@@ -0,0 +1,53 @@
+const navlinks = [
+  {
+    label: "Asosiy",
+    link: "",
+    allowed: ["owner", "admin"],
+  },
+  {
+    label: "Ustozlar",
+    link: "teachers",
+    allowed: ["owner", "admin"],
+  },
+  {
+    label: "Testlar",
+    link: "tests",
+    allowed: ["owner", "admin"],
+  },
+  {
+    label: "Javoblar",
+    link: "submissions",
+    allowed: ["owner", "admin"],
+  },
+  {
+    label: "Natijalar",
+    link: "results",
+    allowed: ["owner", "admin"],
+  },
+  {
+    label: "Havolalar",
+    link: "links",
+    allowed: ["owner", "admin"],
+  },
+  {
+    label: "Statistika",
+    link: "statistics/weekly",
+    allowed: ["owner", "admin"],
+  },
+  {
+    label: "Asboblar",
+    link: "tools",
+    allowed: [],
+  },
+  {
+    label: "Shablonlar",
+    link: "templates",
+    allowed: ["owner", "admin"],
+  },
+];
+
+export const getNavlinksByRole = (role) => {
+  return navlinks.filter(({ allowed }) => allowed.includes(role));
+};
+
+export default navlinks;
---
+++ src/data/questionsType.js
@@ -0,0 +1,42 @@
+// Components
+import Text from "../components/questions/Text";
+import Flowchart from "../components/questions/Flowchart";
+import RadioGroup from "../components/questions/RadioGroup";
+import GridMatching from "../components/questions/GridMatching";
+import CheckboxGroup from "../components/questions/CheckboxGroup";
+import TextDraggable from "../components/questions/TextDraggable";
+
+const questionsType = [
+  {
+    label: "Text",
+    value: "text",
+    component: Text,
+  },
+  {
+    label: "Text draggable",
+    value: "text-draggable",
+    component: TextDraggable,
+  },
+  {
+    label: "Flowchart",
+    value: "flowchart",
+    component: Flowchart,
+  },
+  {
+    label: "Radio group",
+    value: "radio-group",
+    component: RadioGroup,
+  },
+  {
+    label: "Checkbox group",
+    value: "checkbox-group",
+    component: CheckboxGroup,
+  },
+  {
+    label: "Grid matching",
+    value: "grid-matching",
+    component: GridMatching,
+  },
+];
+
+export default questionsType;
